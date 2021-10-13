99b56d52b3a216c9577ab527b26551e97949b66a
Chris Shimmin <shimmin.christopher@gmail.com>
2021-10-13 06:25:38 -0400

    add GA code

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,7 +84,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `G-16WL9W89J0`,
       },
     },
     {
