7c68c524b0269f0b0c67d9a5e26d383f3a96c42a
Chris Shimmin <cshimmin@TCAdmins-MacBook-Pro-5.local>
2022-03-22 21:47:02 -0400

    update the rules

--- src/pages/rules.js
+++ src/pages/rules.js
@@ -26,41 +26,68 @@ const TopicPage = ({ data, location }) => {
       <LandingPageHeader title="Rules" />
       {/* player1 and player3 are on the same team */}
       <LandingPageBody>
-        <ul>
-          <li>There are 14 matches for 14 points. </li>
-          <li>A team needs 7.5 points to win</li>
-          <li>Each match is 1 point. If it's halved, both teams get ½ point</li>
-          <li>
-            Each nine holes has two 2v2 matches, except for the last 9 holes.
-            (Four 1v1 matches)
-          </li>
-          <li>We will rotate the carts at the turn</li>
-          <li>Side bets encouraged</li>
-        </ul>
+        <h2>Scoring</h2>
+        <p>
+          Each match is worth one point, with matches ending in a tie worth ½
+          point to each side. The first team to reach 7 ½ points wins the BFBH
+          Cup. If the matches end in a 7-7 tie, the team holding the BFBH Cup
+          retains it.
+        </p>
+        <p>We will rotate the carts at the turn</p>
+        <p>Side bets encouraged</p>
+
+        <h3>Points</h3>
+        <p>
+          The two days of the matches consist of 14 total matches, each of which
+          is worth one point. There are no extra holes in the BFBH Cup matches.
+          Should the two sides be tied after 18 holes, each side earns a half
+          point.
+        </p>
+        <p>
+          Each nine holes has two 2v2 matches, except for the last 9 holes.
+          (Four 1v1 matches)
+        </p>
+
+        <h3>Concession</h3>
+        <p>
+          Unlike stroke play, players don't have to complete each hole in match
+          play. If a player concedes a stroke – almost always a putt – to his
+          opponent, the opponent picks up his ball, takes the score he would
+          have made on the next stroke and moves on to the next hole.
+        </p>
 
         <h2>Formats</h2>
-        <ul>
-          <li>
-            <b>Bramble. Full handicap</b>: both players tee off, choose the
-            better tee shot and then finish the hole with their own
-            ball/score.Both players' scores will be used for the match play.
-          </li>
-          <li>
-            <b>Scramble</b>: normal scramble with the 2 players, the average of
-            the 2 players' handicaps will be used. Ex. an 18 with a 10 would be
-            a 14.
-          </li>
-          <li>
-            <b>1 best ball</b>: each player plays their own ball, whoever has
-            the better handicapped score of the 2 will be score used for that
-            hole
-          </li>
-          <li>
-            <b>Bramble alternate. Average handicap</b>: both players tee off.
-            Whoever's tee shot is not being used will hit the second shot.
-            Alternate until the ball is holed.
-          </li>
-        </ul>
+
+        <h3>Best Ball. Full Handicap</h3>
+        <p>
+          Each player plays their own ball, whoever has the better handicapped
+          score of the 2 will be score used for that hole.
+        </p>
+
+        <h3>Scramble</h3>
+        <p>
+          Normal scramble with the 2 players, the average of the 2 players'
+          handicaps will be used. Ex. an 18 with a 10 would be a 14.
+        </p>
+
+        <h3>Two-Ball Bramble. Full Handicap</h3>
+        <p>
+          Both players tee off. Players choose the better tee shot then continue
+          the hole with their own ball/score. Both players' scores will be used
+          for the match play.
+        </p>
+
+        <h3>Bramble Alternate. Average Handicap</h3>
+        <p>
+          Both players tee off. Whoever's tee shot is not being used will hit
+          the second shot. Alternate until the ball is holed.
+        </p>
+
+        <h3>Singles</h3>
+        <p>
+          The player with the lower score on each hole wins that hole. If their
+          scores are tied, the hole is halved.
+        </p>
       </LandingPageBody>
     </Layout>
   )
