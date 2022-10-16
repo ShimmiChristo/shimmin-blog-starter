c37db84a397d0de150a63a59311cb6e7d6495803
shimmiChristo <shimmin.christopher@gmail.com>
2022-10-16 08:56:41 -0400

    day 2 update

--- src/components/highlights/day-hightlights.js
+++ src/components/highlights/day-hightlights.js
@@ -6,20 +6,146 @@ function HighlightDay() {
     <>
       <div className="container my-3 mb-lg-5 col-lg-8">
         <div className="row text-left justify-content-start">
+          <h2 className="bold h1 text-center">Day 2</h2>
+          <p>
+            Day 2 has 2 rounds left to play.{" "}
+            <a
+              href="https://gulllakeview.com/golf/golf-courses/gull-lake-view-east/"
+              className="underline"
+            >
+              Gull Lake East
+            </a>{" "}
+            and{" "}
+            <a
+              href="https://gulllakeview.com/golf/golf-courses/gull-lake-view-west/"
+              className="underline"
+            >
+              {" "}
+              Gull Lake West
+            </a>
+            . Both courses are around 6k yards and wooden on both sides.
+            <br />
+            The games being played are 1 ball bramble, alternate shot, scramble,
+            and singles matches.
+            <br />
+            Green starts day 2 with a 5 point lead and only{" "}
+            <span className="bold font-italic">needs 5.5 more points</span> to
+            steal the cup.
+            <br />
+          </p>
+          <h3 className="h4">Round 3.1 - 1 Ball Bramble</h3>
+          <p>
+            Teams originally thought the 1 ball bramble would favor more to the
+            blue team. This proved to be false. The Green Team won all 3 matches
+            on the first nine holes.
+          </p>
+          <p>
+            <span className="block bold green">Green Team - 11.5</span>
+            <span className="block bold blue">Blue Team - 3.5</span>
+          </p>
+          <h3 className="h4">Round 3.2 - Alternate Shot</h3>
+          <p>
+            Alternate shot is a true test of partnership. Green came out hot
+            again with a <span className="bold font-italic">5&3</span> by{" "}
+            <Link to="/players/matt" className="green underline">
+              Matt
+            </Link>{" "}
+            and{" "}
+            <Link to="/players/gordon" className="green underline">
+              Gordon
+            </Link>{" "}
+            over{" "}
+            <Link to="/players/cam" className="blue underline">
+              Cam
+            </Link>{" "}
+            and{" "}
+            <Link to="/players/rj" className="blue underline">
+              RJ
+            </Link>
+            . Then another <span className="bold font-italic">3&2</span> victory
+            by{" "}
+            <Link to="/players/travis" className="green underline">
+              Travis
+            </Link>{" "}
+            and{" "}
+            <Link to="/players/derek" className="green underline">
+              Derek
+            </Link>{" "}
+            over{" "}
+            <Link to="/players/evan" className="blue underline">
+              Evan
+            </Link>{" "}
+            and{" "}
+            <Link to="/players/dan" className="blue underline">
+              Dan
+            </Link>
+            .
+          </p>
+          <p>
+            Now, green only needs a half point. With this as the last match, it
+            came down to the last putt on the last hole. It was{" "}
+            <Link to="/players/craig" className="blue underline">
+              Craig
+            </Link>{" "}
+            with a 3 foot putt and a chance to extend the cup. With everyone
+            watching and{" "}
+            <Link to="/players/matt" className="green underline">
+              Matt
+            </Link>{" "}
+            recording, he putts and it breaks left, missing the cup completely.
+            He drops to his knees and the green team jumps in celebration. Green
+            wins. Green wins. Green wins. (video is available on the{" "}
+            <a href="https://www.instagram.com/bfbhcup/"> BFBH Cup Instagram</a>
+            )
+          </p>
+          <p>
+            <span className="block bold green">Green Team - 14</span>
+            <span className="block bold blue">Blue Team - 4</span>
+          </p>
+          <h3 className="h4">Round 4.1 - Scramble</h3>
+          <p>
+            The last round is at Gull Lake West. It doesn't matter at this point
+            but everyone still plays for pride.
+            <br />
+            Blue team finally shows up, taking 2 of the 3 points in the scramble
+            play. Blue team captain,{" "}
+            <Link to="/players/dylan" className="blue underline">
+              Dylan
+            </Link>
+            , finally gets his first win of the weekend.
+          </p>
+          <h3 className="h4">Round 4.2 - Singles</h3>
+          <p>
+            Perhaps the only highlight for Blue was the singles matches. They
+            end up winning 5 of the 6 matches, including Blue captain over the
+            Green captain.
+          </p>
+          <p>
+            <span className="block bold green">Green Team - 16</span>
+            <span className="block bold blue">Blue Team - 11</span>
+          </p>
+          <p>
+            2022 introduced new faces to the cup. With those faces, came an
+            array of talent. This year it was all Green Team. They came out hot
+            and they rode that momentum to victory. Some may say the teams were
+            uneven. Maybe Blue just played poorly and Green showed up. The
+            singles matches were not a factor this year but the losses will be
+            remembered for the next matchup.
+          </p>
+          <p>Now, go get some practice Blue team. See you next year. 💚💙</p>
+
           <h2 className="bold h1 text-center">Day 1</h2>
           <p>
             The tournament started at arguably the nicest course of the weekend,{" "}
             <a
-              className="underline"
               href="https://gulllakeview.com/golf/stoatin-brae/"
+              className="underline"
             >
               Stoatin Brae
             </a>
             . The first nine holes was 1 best ball.
           </p>
-
-          <h3>Round 1.1 - 1 Best Ball</h3>
-
+          <h3 className="h4">Round 1.1 - 1 Best Ball</h3>
           <p>
             <Link to="/players/dylan" className="blue underline">
               Dylan
@@ -37,7 +163,7 @@ function HighlightDay() {
               Gordon
             </Link>{" "}
             and could not recovery, giving the first point to Green with a 4&3
-            victory early. The other two matches came down to the last hole with{" "}
+            victory early. The other two matches came down to the last hole.{" "}
             <Link to="/players/craig" className="blue underline">
               Craig
             </Link>{" "}
@@ -45,7 +171,7 @@ function HighlightDay() {
             <Link to="/players/dan" className="blue underline">
               Dan
             </Link>{" "}
-            beating{" "}
+            beat{" "}
             <Link to="/players/curtis" className="green underline">
               Curtis
             </Link>{" "}
@@ -72,46 +198,83 @@ function HighlightDay() {
             .
           </p>
           <p>
-            After Round 1.1, it was{" "}
-            <span className="bold green">2-1 Green Team.</span>
+            After Round 1.1, the score is:
+            <br />
+            <span className="block bold green">Green Team - 2</span>
+            <span className="block bold blue">Blue Team - 1</span>
           </p>
 
-          <h3>Round 1.2 - 2 Best Ball</h3>
-
+          <h3 className="h4">Round 1.2 - 2 Best Ball</h3>
           <p>
             Round 1.2 was the second half of Stoatin Brae and the game-play was
             2 best ball. In this format, the ball from each player are used and
             totaled. The lower totaled score wins.
           </p>
-
           <p>
             After a slight rotation of the carts, the first match still found
-            the captains facing each other. Matt came out on top again. Another
-            point for the Green.
+            the captains facing each other.{" "}
+            <Link to="/players/matt" className="green underline">
+              Matt
+            </Link>{" "}
+            came out on top again. Another point for the Green.
           </p>
           <p>
-            In perhaps the match of the weekend, RJ and Evan found themselves
-            down 4 points with 5 holes to play against Gordon and Derek. In an
-            absolute thriller, down to the wire, RJ and Evan rallied and won 5
-            straight holes to seal the victory.
+            In perhaps the match of the weekend,{" "}
+            <Link to="/players/rj" className="blue underline">
+              RJ
+            </Link>{" "}
+            and{" "}
+            <Link to="/players/evan" className="blue underline">
+              Evan
+            </Link>{" "}
+            found themselves down 4 points with 5 holes to play against{" "}
+            <Link to="/players/gordon" className="green underline">
+              Gordon
+            </Link>{" "}
+            and{" "}
+            <Link to="/players/derek" className="green underline">
+              Derek
+            </Link>
+            . In an absolute thriller, down to the wire,{" "}
+            <Link to="/players/rj" className="blue underline">
+              RJ
+            </Link>{" "}
+            and{" "}
+            <Link to="/players/evan" className="blue underline">
+              Evan
+            </Link>{" "}
+            rallied and won 5 straight holes to seal the victory.
           </p>
           <p>
             After a lost score card, we don't know the individual scores, but
-            Travis and Jake beat Cam and Craig to take 1 more point for Green.
+            <Link to="/players/travis" className="green underline">
+              Travis
+            </Link>{" "}
+            and{" "}
+            <Link to="/players/jake" className="green underline">
+              Jake
+            </Link>{" "}
+            beat{" "}
+            <Link to="/players/cam" className="blue underline">
+              Cam
+            </Link>{" "}
+            and{" "}
+            <Link to="/players/craig" className="blue underline">
+              Craig
+            </Link>{" "}
+            to take 1 more point for Green.
           </p>
           <p>
             After the first 18 holes,{" "}
             <span className="bold green">Green leads 4 points to 2.</span>
           </p>
-
-          <h3>Round 2.1 - 2 Ball Bramble</h3>
-
+          <h3 className="h4">Round 2.1 - 2 Ball Bramble</h3>
           <p>
             For the second 18, the groups scooted on over to another Gull Lake
             View golf course,{" "}
             <a
-              className="underline"
               href="https://gulllakeview.com/golf/golf-courses/stonehedge-south/"
+              className="underline"
             >
               Stonehedge South
             </a>
@@ -120,33 +283,120 @@ function HighlightDay() {
           </p>
           <p>Two of the three matches were nail-biters.</p>
           <p>
-            Gordon and Travis can't find a way to score on the last hole and
-            lose by one to RJ and Craig.
+            <Link to="/players/gordon" className="green underline">
+              Gordon
+            </Link>{" "}
+            and{" "}
+            <Link to="/players/travis" className="green underline">
+              Travis
+            </Link>{" "}
+            can't find a way to score on the last hole and lose by one to{" "}
+            <Link to="/players/rj" className="blue underline">
+              RJ
+            </Link>{" "}
+            and{" "}
+            <Link to="/players/craig" className="blue underline">
+              Craig
+            </Link>
+            .
           </p>
           <p>
-            Jake and Derek vs Dylan and Cam ended in a tie, but every hole
-            besides #9 was scored. This was an exciting match that ended in
-            All-Square.
+            <Link to="/players/jake" className="green underline">
+              Jake
+            </Link>{" "}
+            and{" "}
+            <Link to="/players/derek" className="green underline">
+              Derek
+            </Link>{" "}
+            vs{" "}
+            <Link to="/players/dylan" className="blue underline">
+              Dylan
+            </Link>{" "}
+            and{" "}
+            <Link to="/players/cam" className="blue underline">
+              Cam
+            </Link>{" "}
+            ended in a tie, but every hole besides #9 was scored. This was an
+            exciting match that ended in All-Square.
           </p>
           <p>
             Winning 5 straight holes is something we hadn't seen in the BFBH
-            Cup, until today, and we saw it twice. Once with RJ and Evan's
-            come-from-behind victory and the other time was this match with Matt
-            and Curtis winning 5 straight holes against Evan and Dan.
+            Cup, until today, and we saw it twice. Once with{" "}
+            <Link to="/players/rj" className="blue underline">
+              RJ
+            </Link>{" "}
+            and{" "}
+            <Link to="/players/evan" className="blue underline">
+              Evan's
+            </Link>{" "}
+            come-from-behind victory and the other time was this match with{" "}
+            <Link to="/players/matt" className="green underline">
+              Matt
+            </Link>{" "}
+            and{" "}
+            <Link to="/players/curtis" className="green underline">
+              Curtis
+            </Link>{" "}
+            winning 5 straight holes against Evan and{" "}
+            <Link to="/players/dan" className="blue underline">
+              Dan
+            </Link>
+            .
           </p>
           <p>
             <span className="block bold green">Green Team - 5.5</span>
             <span className="block bold blue">Blue Team - 3.5</span>
           </p>
-          <h3>Round 2.2 - Scramble</h3>
+          <h3 className="h4">Round 2.2 - Scramble</h3>
           <p>
             The second half of Stonehedge South, the wheels fell off for Blue.
             They lost every match on the back nine.
           </p>
           <p>
-            Gordon and Jake with a 3&2 victory over Cam and Craig. Travis and
-            Matt was 2&1 over Dan and RJ. Dylan and Evan concede on the fifth
-            hole against Derek and Curtis (final score 4&3).
+            <Link to="/players/gordon" className="green underline">
+              Gordon
+            </Link>{" "}
+            and{" "}
+            <Link to="/players/jake" className="green underline">
+              Jake
+            </Link>{" "}
+            with a 3&2 victory over{" "}
+            <Link to="/players/cam" className="blue underline">
+              Cam
+            </Link>{" "}
+            and{" "}
+            <Link to="/players/craig" className="blue underline">
+              Craig
+            </Link>
+            .{" "}
+            <Link to="/players/travis" className="green underline">
+              Travis
+            </Link>{" "}
+            and Matt was 2&1 over{" "}
+            <Link to="/players/dan" className="blue underline">
+              Dan
+            </Link>{" "}
+            and{" "}
+            <Link to="/players/rj" className="blue underline">
+              RJ
+            </Link>
+            .{" "}
+            <Link to="/players/dylan" className="blue underline">
+              Dylan
+            </Link>{" "}
+            and{" "}
+            <Link to="/players/evan" className="blue underline">
+              Evan
+            </Link>{" "}
+            concede on the fifth hole against{" "}
+            <Link to="/players/derek" className="green underline">
+              Derek
+            </Link>{" "}
+            and{" "}
+            <Link to="/players/curtis" className="green underline">
+              Curtis
+            </Link>{" "}
+            (final score 4&3).
           </p>
           <p>
             After day 1 and 12 matches later, the score is:
