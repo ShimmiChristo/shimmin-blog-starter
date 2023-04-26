1152c86929b705b6e9152dcd31cd3df392f3c7ae
shimmiChristo <shimmin.christopher@gmail.com>
2023-04-26 07:54:12 -0400

    remove gameplay records

--- src/components/records-partners.js
+++ src/components/records-partners.js
@@ -17,28 +17,9 @@ function RecordPartners({ playerData }) {
           elem[recordParam].forEach(item => {
             const name = item.name
             if (recordObj[name]) {
-              recordObj[name].bestBall.wins += item.bestBall.wins
-              recordObj[name].bestBall.ties += item.bestBall.ties
-              recordObj[name].bestBall.losses += item.bestBall.losses
-              recordObj[name].bramble.wins += item.bramble.wins
-              recordObj[name].bramble.ties += item.bramble.ties
-              recordObj[name].bramble.losses += item.bramble.losses
-              recordObj[name].brambleAlternate.wins +=
-                item.brambleAlternate.wins
-              recordObj[name].brambleAlternate.ties +=
-                item.brambleAlternate.ties
-              recordObj[name].brambleAlternate.losses +=
-                item.brambleAlternate.losses
-              recordObj[name].scramble.wins += item.scramble.wins
-              recordObj[name].scramble.ties += item.scramble.ties
-              recordObj[name].scramble.losses += item.scramble.losses
-              recordObj[name].twoBestBall.wins += item.twoBestBall.wins
-              recordObj[name].twoBestBall.ties += item.twoBestBall.ties
-              recordObj[name].twoBestBall.losses += item.twoBestBall.losses
-              recordObj[name].twoBallBramble.wins += item.twoBallBramble.wins
-              recordObj[name].twoBallBramble.ties += item.twoBallBramble.ties
-              recordObj[name].twoBallBramble.losses +=
-                item.twoBallBramble.losses
+              recordObj[name].wins += item.wins
+              recordObj[name].ties += item.ties
+              recordObj[name].losses += item.losses
             } else {
               //! add new contact
               recordObj[name] = { ...item }
@@ -51,7 +32,7 @@ function RecordPartners({ playerData }) {
   }
 
   var partnerRecords = getGameRecord(playerData, "partners")
-  var opponentsRecords = getGameRecord(playerData, "opponents")
+  // var opponentsRecords = getGameRecord(playerData, "opponents")
 
   return (
     <div className="row">
@@ -62,29 +43,7 @@ function RecordPartners({ playerData }) {
         >
           <div className="text-center w-100">{key}</div>
           <div className="text-center w-100">
-            {value.bestBall.wins}-{value.bestBall.ties}-{value.bestBall.losses}
-          </div>
-          <div className="text-center w-100">
-            {value.twoBestBall.wins}-{value.twoBestBall.ties}-
-            {value.twoBestBall.losses}
-          </div>
-          <div className="text-center w-100">
-            {value.bramble.wins}-{value.bramble.ties}-{value.bramble.losses}
-          </div>
-          <div className="text-center w-100">
-            {value.twoBallBramble.wins}-{value.twoBallBramble.ties}-
-            {value.twoBallBramble.losses}
-          </div>
-          <div className="text-center w-100">
-            {value.alternate.wins}-{value.alternate.ties}-
-            {value.alternate.losses}
-          </div>
-          <div className="text-center w-100">
-            {value.brambleAlternate.wins}-{value.brambleAlternate.ties}-
-            {value.brambleAlternate.losses}
-          </div>
-          <div className="text-center w-100">
-            {value.scramble.wins}-{value.scramble.ties}-{value.scramble.losses}
+            {value.wins}-{value.ties}-{value.losses}
           </div>
         </div>
       ))}
