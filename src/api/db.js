712a0e2573da1757748b8a47f6e12400dae69579
shimmiChristo <shimmin.christopher@gmail.com>
2023-09-29 15:31:15 -0400

    add test

--- src/api/db.js
+++ src/api/db.js
@@ -1,18 +1,21 @@
-import { createRequire } from "module"
-const require = createRequire(import.meta.url)
+// import { createRequire } from "module"
+// const require = createRequire(import.meta.url)
+import { PlayerInfoUpdate } from "../hooks/get-player-info-UPDATE"
+const playersUpdateJson = PlayerInfoUpdate()
 
 export default function handler(req, res) {
   if (req.httpMethod === "GET") {
     try {
       // Process the GET request as needed
-      const data = require("../data/playersUpdate.json")
+      // const data = require("../data/playersUpdate.json")
+      const data = playersUpdateJson();
 
       // Add CORS headers
       const headers = {
         "Access-Control-Allow-Origin": "*", // Replace * with the appropriate domain
         "Access-Control-Allow-Headers": "Content-Type",
+        "Content-Type": "application/json"
       }
-      console.log('JSON.stringify(data) - ', JSON.stringify(data));
       // Return the data as the response
       return {
         statusCode: 200,
