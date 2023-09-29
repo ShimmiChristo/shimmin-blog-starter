f37d88ce7f9dba1dfbd640708d2d33006786d6cd
shimmiChristo <shimmin.christopher@gmail.com>
2023-09-29 14:58:06 -0400

    add test

--- src/api/db.js
+++ src/api/db.js
@@ -1,34 +1,34 @@
-// import { createRequire } from "module"
-// const require = createRequire(import.meta.url)
-
-// export default async function handler(req, res) {
-//   if (req.httpMethod === "GET") {
-//     try {
-//       // Process the GET request as needed
-//       const data = require("../data/playersUpdate.json")
-
-//       // Add CORS headers
-//       const headers = {
-//         "Access-Control-Allow-Origin": "*", // Replace * with the appropriate domain
-//         "Access-Control-Allow-Headers": "Content-Type",
-//       }
-
-//       // Return the data as the response
-//       return {
-//         statusCode: 200,
-//         headers,
-//         body: JSON.stringify(data),
-//       }
-//     } catch (error) {
-//       // Return an error response if there was an issue processing the request
-//       return {
-//         statusCode: 500,
-//         body: JSON.stringify({ error: "Failed to process GET request" }),
-//       }
-//     }
-//   }
-// }
+import { createRequire } from "module"
+const require = createRequire(import.meta.url)
 
 export default function handler(req, res) {
-  res.status(200).json({ hello: `world` })
+  if (req.httpMethod === "GET") {
+    try {
+      // Process the GET request as needed
+      const data = require("../data/playersUpdate.json")
+
+      // Add CORS headers
+      const headers = {
+        "Access-Control-Allow-Origin": "*", // Replace * with the appropriate domain
+        "Access-Control-Allow-Headers": "Content-Type",
+      }
+      console.log('JSON.stringify(data) - ', JSON.stringify(data));
+      // Return the data as the response
+      return {
+        statusCode: 200,
+        headers,
+        body: JSON.stringify(data),
+      }
+    } catch (error) {
+      // Return an error response if there was an issue processing the request
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error: "Failed to process GET request" }),
+      }
+    }
+  }
 }
+
+// export default function handler(req, res) {
+//   res.status(200).json({ hello: `world` })
+// }
