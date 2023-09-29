c23ad0ef9d368902cccf687413dfa286094cda5f
shimmiChristo <shimmin.christopher@gmail.com>
2023-09-29 19:16:58 -0400

    update api

--- src/api/db.js
+++ src/api/db.js
@@ -2,16 +2,6 @@ import { createRequire } from "module"
 const require = createRequire(import.meta.url)
 
 export default function handler(req, res) {
-  if (req.httpMethod === "GET") {
-    try {
-      const data = require("../data/playersUpdate.json")
-      res.status(200).json(data)
-    } catch (error) {
-      // Return an error response if there was an issue processing the request
-      return {
-        statusCode: 500,
-        body: JSON.stringify({ error: "Failed to process GET request" }),
-      }
-    }
-  }
+  const data = require("../data/playersUpdate.json")
+  res.status(200).json(data)
 }
