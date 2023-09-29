79e6b219908eb2d8579903520cc4348ad23fbaa5
shimmiChristo <shimmin.christopher@gmail.com>
2023-09-29 16:06:56 -0400

    testing

--- src/api/test.js
+++ src/api/test.js
@@ -3,32 +3,8 @@
 // import { PlayerInfoUpdate } from "../../src/hooks/get-player-info-UPDATE"
 
 export default function handler(req, res) {
-  // const playersUpdateJson = PlayerInfoUpdate()
-
-  console.log("req - ", req)
-
-  if (req.httpMethod === "GET") {
-    try {
-      // Process the GET request as needed
-      // const data = require("../data/playersUpdate.json")
-      const data = { hello: `world` }
-
-      // Add CORS headers
-      // const headers = {
-      //   "Access-Control-Allow-Origin": "*", // Replace * with the appropriate domain
-      //   "Access-Control-Allow-Headers": "Content-Type",
-      //   "Content-Type": "application/json"
-      // }
-      // Return the data as the response
-      res.status(200).json(data)
-    } catch (error) {
-      // Return an error response if there was an issue processing the request
-      return {
-        statusCode: 500,
-        body: JSON.stringify({ error: "Failed to process GET request" }),
-      }
-    }
-  }
+  const data = { hello: `world` }
+  res.status(200).json(data)
 }
 
 // export default function handler(req, res) {
