d136d02ef414e5a39c0d4c8c09a01f4c8bd9246c
shimmiChristo <shimmin.christopher@gmail.com>
2024-09-30 15:17:11 -0400

    add 2024 pic

--- src/pages/about.js
+++ src/pages/about.js
@@ -47,8 +47,8 @@ const TopicPage = ({ data, location }) => {
         </div>
         <div className="container col-lg-8">
           <StaticImage
-            src="../images/group/2023-group.jpg"
-            alt="2023 bfbh cup players"
+            src="../images/group/2024-group.jpg"
+            alt="2024 bfbh cup players"
             loading="eager"
             placeholder="blurred"
             layout="fullWidth"
