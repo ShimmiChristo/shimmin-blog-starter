6476d529325b5ad5817f8fba99f2076699095d9d
shimmiChristo <shimmin.christopher@gmail.com>
2025-10-06 06:32:09 -0400

    update summary

--- src/components/matches/2025/summary.js
+++ src/components/matches/2025/summary.js
@@ -89,6 +89,35 @@ function MatchSummary2025() {
             first 5 holes to go 5 and 4. This is one of the few 5&4 matches in
             cup history. Green won 5 over through 5 holes.
           </p>
+          <p>
+            The final matches of the day were the scramble matches. The first
+            match was {matt} and {derek} vs {craig} and {evan}. Green team was
+            the high favorite in the match. Green came out early with a 2 hole
+            lead after 2 holes. Blue stayed in the fight though and was dormie
+            after 7 holes. Blue put together back to back net birdies on 8 and 9
+            to push the match and give Blue a 1/2 point.
+          </p>
+          <p>
+            The next match was {dylan} and {cam} vs {stephen} and {chris}. Blue
+            was the slight favorite at the start of the cup. With a record of
+            4-1, {dylan} is the Cup's best scramble player. To litle surprise,
+            Blue was able jump out to a 1 up lead after 4 holes. However, what
+            was a big surprise was {cam} falling in the woods on hole 4 and
+            getting up on banging home a par putt to win the hole. In the end,{" "}
+            {cam} couldn't recover and Green was too much to handle.Green won
+            2&1.
+          </p>
+          <p>
+            The third and final match of the day was {gordon} and {curtis} vs{" "}
+            {rj} and {dan}. The straight up match was back and forth with 5
+            total birdies. With the match all square going into the 9th hole,{" "}
+            {dan} was able to make a clutch birdie putt to win the hole and the
+            the match.
+          </p>
+          <p className="bold">
+            After day 1, Green team has a commanding lead of 8.5 to 3.5. With 15
+            points left, Green needs just 5 points on day 2 to clinch the cup.
+          </p>
         </div>
 
         <div className="container">
