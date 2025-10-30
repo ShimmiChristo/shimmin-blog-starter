66c7c39fed8bbefd2ecd2cc2364465215cc39196
shimmiChristo <shimmin.christopher@gmail.com>
2025-10-30 16:22:47 -0400

    fixing syntax errors

---
+++ src/hooks/fragments/hiddenRiverGolfClub.js
@@ -0,0 +1,118 @@
+import { graphql } from "gatsby"
+
+export const CrookedTreeFragment = graphql`
+  fragment CrookedTreeFragment on CoursesJsonCourse {
+    crookedTree {
+      name
+      link
+      holes {
+        number
+        tees {
+          purple
+          orange
+        }
+        handicap
+        par
+      }
+      totals {
+        tees {
+          purple {
+            out {
+              yardage
+              par
+              index
+              slope
+            }
+            in {
+              yardage
+              par
+              index
+              slope
+            }
+            total {
+              yardage
+              par
+              index
+              slope
+            }
+          }
+          orange {
+            out {
+              yardage
+              par
+              index
+              slope
+            }
+            in {
+              yardage
+              par
+              index
+              slope
+            }
+            total {
+              yardage
+              par
+              index
+              slope
+            }
+          }
+        }
+      }
+      _2025 {
+        holes {
+          number
+          tees {
+            purple
+            orange
+          }
+          handicap
+          par
+        }
+        totals {
+          tees {
+            purple {
+              out {
+                yardage
+                par
+                index
+                slope
+              }
+              in {
+                yardage
+                par
+                index
+                slope
+              }
+              total {
+                yardage
+                par
+                index
+                slope
+              }
+            }
+            orange {
+              out {
+                yardage
+                par
+                index
+                slope
+              }
+              in {
+                yardage
+                par
+                index
+                slope
+              }
+              total {
+                yardage
+                par
+                index
+                slope
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
